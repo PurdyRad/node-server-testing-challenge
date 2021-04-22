4338def03c3ef9eb3d00f7a90fe6ef0a8fbe64e0
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-22 14:37:05 -0400

    created knex file, migrations and seeds

---
+++ data/seeds/01-producers.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('producers').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('producers').insert([
+        {producers_id: 1, name: 'Valentino Khan', genre: 'House'},
+        {producers_id: 2, name: 'Alison Wonderland', genre: 'House'},
+        {producers_id: 3, name: 'Skrillex', genre: 'Fiya'}
+      ]);
+    });
+};
